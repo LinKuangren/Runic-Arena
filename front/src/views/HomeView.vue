0f311bcb21354ddb6d87774bfb40563da1768a97
LinKuangren <romaindanne@gmail.com>
2023-05-05 00:02:29 +0200

    ajout accueil

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -1,9 +1,14 @@
 <template>
-  <main>
-    <h1>AccueilS</h1>
+  <main class="accueil">
+    <router-link to="/cards" class="cta-accueil cta-accueil-1">Cartes</router-link>
+    <div class="div-accueil">
+      <router-link to="/classes" class="cta-accueil cta-accueil-2">Classes</router-link>
+      <router-link to="/types" class="cta-accueil cta-accueil-3">Types</router-link>
+    </div>
+    <router-link to="/competences" class="cta-accueil cta-accueil-1">Compétences</router-link>
   </main>
-</template>
-
+</template>    
+            
 <script>
 
 </script>
